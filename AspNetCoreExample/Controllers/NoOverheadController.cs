1c9a206d6da481ea939d3c121a94d9f688138e7b
彭伟 <pengweiqhca@sina.com>
2022-04-13 16:37:22 +0800

    C# 10

--- AspNetCoreExample/Controllers/NoOverheadController.cs
+++ AspNetCoreExample/Controllers/NoOverheadController.cs
@@ -1,17 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace AspNetCoreExample.Controllers
+namespace AspNetCoreExample.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class NoOverheadController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class NoOverheadController : ControllerBase
-    {
-        [HttpGet]
+    [HttpGet]
 #pragma warning disable CS1998
-        public async Task<ActionResult<IEnumerable<string>>> Get2()
+    public async Task<ActionResult<IEnumerable<string>>> Get2()
 #pragma warning restore CS1998
-        {
-            return new [] {"value1", "value2"};
-        }
+    {
+        return new [] {"value1", "value2"};
     }
 }
--- System.Diagnostics.Runtime/RuntimeMetricsOptions.cs
+++ System.Diagnostics.Runtime/RuntimeMetricsOptions.cs
@@ -1,44 +1,44 @@
-﻿namespace System.Diagnostics.Runtime
+﻿namespace System.Diagnostics.Runtime;
+
+/// <summary>
+/// Options to define the runtime metrics.
+/// </summary>
+public class RuntimeMetricsOptions
 {
     /// <summary>
-    /// Options to define the runtime metrics.
+    /// Gets or sets a value indicating the metrics prefix to use.
     /// </summary>
-    public class RuntimeMetricsOptions
-    {
-        /// <summary>
-        /// Gets or sets a value indicating the metrics prefix to use.
-        /// </summary>
-        public string MetricPrefix { get; set; } = "process.runtime.dotnet.";
+    public string MetricPrefix { get; set; } = "process.runtime.dotnet.";
 
-        /// <summary>
-        /// Gets or sets a value indicating whether enable debugging metrics.
-        /// </summary>
-        public bool EnabledDebuggingMetrics { get; set; }
+    /// <summary>
+    /// Gets or sets a value indicating whether enable debugging metrics.
+    /// </summary>
+    public bool EnabledDebuggingMetrics { get; set; }
 
-        /// <summary>
-        /// Gets or sets a value indicating whether assembly metrics should be collected.
-        /// </summary>
-        public bool? AssembliesEnabled { get; set; }
+    /// <summary>
+    /// Gets or sets a value indicating whether assembly metrics should be collected.
+    /// </summary>
+    public bool? AssembliesEnabled { get; set; }
 
-        /// <summary>
-        /// Gets or sets a value indicating whether contention metrics should be collected.
-        /// </summary>
-        public bool? ContentionEnabled { get; set; }
+    /// <summary>
+    /// Gets or sets a value indicating whether contention metrics should be collected.
+    /// </summary>
+    public bool? ContentionEnabled { get; set; }
 #if NET6_0_OR_GREATER
         /// <summary>
         /// Gets or sets a value indicating whether dns metrics should be collected.
         /// </summary>
         public bool? DnsEnabled { get; set; }
 #endif
-        /// <summary>
-        /// Gets or sets a value indicating whether exception metrics should be collected.
-        /// </summary>
-        public bool? ExceptionsEnabled { get; set; }
+    /// <summary>
+    /// Gets or sets a value indicating whether exception metrics should be collected.
+    /// </summary>
+    public bool? ExceptionsEnabled { get; set; }
 
-        /// <summary>
-        /// Gets or sets a value indicating whether garbage collection metrics should be collected.
-        /// </summary>
-        public bool? GcEnabled { get; set; }
+    /// <summary>
+    /// Gets or sets a value indicating whether garbage collection metrics should be collected.
+    /// </summary>
+    public bool? GcEnabled { get; set; }
 
 #if NET6_0_OR_GREATER
         /// <summary>
@@ -47,10 +47,10 @@
         public bool? JitEnabled { get; set; }
 #endif
 
-        /// <summary>
-        /// Gets or sets a value indicating whether process metrics should be collected.
-        /// </summary>
-        public bool? ProcessEnabled { get; set; }
+    /// <summary>
+    /// Gets or sets a value indicating whether process metrics should be collected.
+    /// </summary>
+    public bool? ProcessEnabled { get; set; }
 
 #if NET6_0_OR_GREATER
         /// <summary>
@@ -58,36 +58,36 @@
         /// </summary>
         public bool? SocketsEnabled { get; set; }
 #endif
-        /// <summary>
-        /// Gets or sets a value indicating whether threading metrics should be collected.
-        /// </summary>
-        public bool? ThreadingEnabled { get; set; }
+    /// <summary>
+    /// Gets or sets a value indicating whether threading metrics should be collected.
+    /// </summary>
+    public bool? ThreadingEnabled { get; set; }
 
-        private bool IsAllEnabled => AssembliesEnabled == null
-                                     && ContentionEnabled == null
+    private bool IsAllEnabled => AssembliesEnabled == null
+                                 && ContentionEnabled == null
 #if NET6_0_OR_GREATER
                                      && DnsEnabled == null
 #endif
-                                     && ExceptionsEnabled == null
-                                     && GcEnabled == null
+                                 && ExceptionsEnabled == null
+                                 && GcEnabled == null
 #if NET6_0_OR_GREATER
                                      && JitEnabled == null
 #endif
-                                     && ProcessEnabled == null
+                                 && ProcessEnabled == null
 #if NET6_0_OR_GREATER
                                      && SocketsEnabled == null
 #endif
-                                     && ThreadingEnabled == null;
+                                 && ThreadingEnabled == null;
 
-        /// <summary>
-        /// Gets a value indicating whether assembly metrics is enabled.
-        /// </summary>
-        internal bool IsAssembliesEnabled => AssembliesEnabled == true || IsAllEnabled;
+    /// <summary>
+    /// Gets a value indicating whether assembly metrics is enabled.
+    /// </summary>
+    internal bool IsAssembliesEnabled => AssembliesEnabled == true || IsAllEnabled;
 
-        /// <summary>
-        /// Gets a value indicating whether contention metrics is enabled.
-        /// </summary>
-        internal bool IsContentionEnabled => ContentionEnabled == true || IsAllEnabled;
+    /// <summary>
+    /// Gets a value indicating whether contention metrics is enabled.
+    /// </summary>
+    internal bool IsContentionEnabled => ContentionEnabled == true || IsAllEnabled;
 
 #if NET6_0_OR_GREATER
         /// <summary>
@@ -95,15 +95,15 @@
         /// </summary>
         internal bool IsDnsEnabled => DnsEnabled == true || IsAllEnabled;
 #endif
-        /// <summary>
-        /// Gets a value indicating whether exception metrics is enabled.
-        /// </summary>
-        internal bool IsExceptionsEnabled => ExceptionsEnabled == true || IsAllEnabled;
+    /// <summary>
+    /// Gets a value indicating whether exception metrics is enabled.
+    /// </summary>
+    internal bool IsExceptionsEnabled => ExceptionsEnabled == true || IsAllEnabled;
 
-        /// <summary>
-        /// Gets a value indicating whether garbage collection metrics is enabled.
-        /// </summary>
-        internal bool IsGcEnabled => GcEnabled == true || IsAllEnabled;
+    /// <summary>
+    /// Gets a value indicating whether garbage collection metrics is enabled.
+    /// </summary>
+    internal bool IsGcEnabled => GcEnabled == true || IsAllEnabled;
 
 #if NET6_0_OR_GREATER
         /// <summary>
@@ -112,10 +112,10 @@
         internal bool IsJitEnabled => JitEnabled == true || IsAllEnabled;
 #endif
 
-        /// <summary>
-        /// Gets a value indicating whether process metrics is enabled.
-        /// </summary>
-        internal bool IsProcessEnabled => ProcessEnabled == true || IsAllEnabled;
+    /// <summary>
+    /// Gets a value indicating whether process metrics is enabled.
+    /// </summary>
+    internal bool IsProcessEnabled => ProcessEnabled == true || IsAllEnabled;
 
 #if NET6_0_OR_GREATER
         /// <summary>
@@ -125,16 +125,16 @@
             SocketsEnabled == true || IsAllEnabled;
 #endif
 
-        /// <summary>
-        /// Gets a value indicating whether threading metrics is enabled.
-        /// </summary>
-        internal bool IsThreadingEnabled => ThreadingEnabled == true || IsAllEnabled;
+    /// <summary>
+    /// Gets a value indicating whether threading metrics is enabled.
+    /// </summary>
+    internal bool IsThreadingEnabled => ThreadingEnabled == true || IsAllEnabled;
 
-        /// <summary>
-        /// Gets or sets a value indicating whether enable listen native event source.
-        /// </summary>
-        /// <remarks>If true, metrics will include more detail info.</remarks>
-        public bool EnabledNativeRuntime { get; set; }
+    /// <summary>
+    /// Gets or sets a value indicating whether enable listen native event source.
+    /// </summary>
+    /// <remarks>If true, metrics will include more detail info.</remarks>
+    public bool EnabledNativeRuntime { get; set; }
 #if NETCOREAPP
         /// <summary>
         /// Gets or sets a value indicating whether enable listen system runtime event source.
@@ -142,11 +142,12 @@
         /// <remarks>If true, metrics will include more detail info.</remarks>
         public bool EnabledSystemRuntime { get; set; }
 #else
-        /// <summary>
-        /// Gets or sets the name of the session to open. Should be unique across the machine.
-        /// </summary>
-        /// <remarks>If EnabledNativeRuntime is true, EtwSessionName must have value. Administrator rights required to start ETW.</remarks>
-        public string? EtwSessionName { get; set; }
+    /// <summary>
+    /// Gets or sets the name of the session to open. Should be unique across the machine.
+    /// </summary>
+    /// <remarks>If EnabledNativeRuntime is true, EtwSessionName must have value. Administrator rights required to start ETW.</remarks>
+    public string? EtwSessionName { get; set; }
+
+    public Microsoft.Diagnostics.Tracing.Session.TraceEventProviderOptions ProviderOptions { get; } = new();
 #endif
-    }
 }

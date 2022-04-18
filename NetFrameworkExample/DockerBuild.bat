508a5020b5fb7821d00440e5f003d27e7ef27274
彭伟 <pengweiqhca@sina.com>
2022-04-18 16:38:35 +0800

    isolation=process

--- NetFrameworkExample/DockerBuild.bat
+++ NetFrameworkExample/DockerBuild.bat
@@ -3,4 +3,4 @@ docker rm netframeworkexample
 docker rmi netframeworkexample
 dotnet build
 docker build -t netframeworkexample .
-docker run -d --name netframeworkexample -p 9464:9464 -p 9465:9465 netframeworkexample
+docker run -d --name netframeworkexample --isolation=process -p 9464:9464 -p 9465:9465 netframeworkexample

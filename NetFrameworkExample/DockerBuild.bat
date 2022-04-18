f140753ee8ea0301092e9efc857d86656524abfb
彭伟 <pengweiqhca@sina.com>
2022-04-18 14:43:18 +0800

    Add windows container

---
+++ NetFrameworkExample/DockerBuild.bat
@@ -0,0 +1,6 @@
+﻿docker kill netframeworkexample
+docker rm netframeworkexample
+docker rmi netframeworkexample
+dotnet build
+docker build -t netframeworkexample .
+docker run -d --name netframeworkexample -p 9464:9464 -p 9465:9465 netframeworkexample

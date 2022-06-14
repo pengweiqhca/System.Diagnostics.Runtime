83b1f40e6aa9dafa21ec39d57e5c92decc124317
彭伟 <pengweiqhca@sina.com>
2022-06-14 13:48:02 +0800

    Support docker running.

---
+++ NetFrameworkExample/HttpListenerHostedService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Diagnostics.Runtime;
+using System.Net;
+
+namespace NetFrameworkExample;
+
+public class HttpListenerHostedService : BackgroundService, IDisposable
+{
+    private readonly HttpListener _httpListener = new() { Prefixes = { "http://localhost:9465/" } };
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _httpListener.Start();
+
+        var client = new HttpClient { Timeout = TimeSpan.FromSeconds(10) };
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var context = await _httpListener.GetContextAsync().ConfigureAwait(false);
+
+            _ = Task.Run(async () =>
+            {
+                await Simulate.Invoke(true, true, true, true, true, () => client).ConfigureAwait(false);
+
+                context.Response.Close();
+            }, stoppingToken);
+        }
+    }
+}
--- NetFrameworkExample/Program.cs
+++ NetFrameworkExample/Program.cs
@@ -1,40 +1,16 @@
-﻿using System.Diagnostics.Runtime;
-using System.Net;
-using OpenTelemetry;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NetFrameworkExample;
+using System.Diagnostics.Runtime;
 using OpenTelemetry.Metrics;
 
-using var meterProvider = Sdk.CreateMeterProviderBuilder()
-    .AddRuntimeInstrumentation()
-    .AddView(instrument =>
-        instrument.Name is "process.runtime.dotnet.gc.collection.seconds" or "process.runtime.dotnet.gc.pause.seconds"
-            ? new ExplicitBucketHistogramConfiguration { Boundaries = new[] { 0.001, 0.01, 0.05, 0.1, 0.5, 1, 10 } }
-            : null)
-    .AddPrometheusExporter()
-    .Build();
-
-using var httpListener = new HttpListener();
-
-httpListener.Prefixes.Add("http://localhost:9465/");
-
-Task.Run(async () =>
-{
-    httpListener.Start();
-
-    var client = new HttpClient();
-
-    while (true)
-    {
-        var context = await httpListener.GetContextAsync().ConfigureAwait(false);
-
-        _ = Task.Run(async () =>
-        {
-            await Simulate.Invoke(true, true, true, true, true, () => client).ConfigureAwait(false);
-
-            context.Response.Close();
-        });
-    }
-});
-
-Console.WriteLine("Press ENTER to exit...");
-
-Console.ReadLine();
+await new HostBuilder().ConfigureServices(services => services
+        .AddHostedService<HttpListenerHostedService>()
+        .AddOpenTelemetryMetrics(builder => builder.AddRuntimeInstrumentation().AddView(instrument =>
+                instrument.Name is "process.runtime.dotnet.gc.collection.seconds" or
+                    "process.runtime.dotnet.gc.pause.seconds"
+                    ? new ExplicitBucketHistogramConfiguration
+                        { Boundaries = new[] { 0.001, 0.01, 0.05, 0.1, 0.5, 1, 10 } }
+                    : null)
+            .AddPrometheusExporter()))
+    .Build().RunAsync().ConfigureAwait(false);
